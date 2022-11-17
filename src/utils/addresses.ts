b3efc456738ade6d103a4cb8d154a3947d4016e5
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2022-11-17 12:02:41 -0300

    Lowercase addresses

--- src/utils/addresses.ts
+++ src/utils/addresses.ts
@@ -1,2 +1,2 @@
-export const LANDRegistry = '0x25b6B4bac4aDB582a0ABd475439dA6730777Fbf7'
-export const EstateRegistry = '0xC9A46712E6913c24d15b46fF12221a79c4e251DC'
+export const LANDRegistry = '0x25b6b4bac4adb582a0abd475439da6730777fbf7'
+export const EstateRegistry = '0xc9a46712e6913c24d15b46ff12221a79c4e251dc'
